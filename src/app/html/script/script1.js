7324da5f38995d8b98a092012985071cb9606e5e
Priyadharshini115 <priyadharshinibarath115@gmail.com>
2023-12-14 10:50:20 +0530

    test

--- src/app/html/script/script1.js
+++ src/app/html/script/script1.js
@@ -1,3 +1,23 @@
 function data(){
-    alert('Please Enter Credentials');
+    const userName=document.getElementById("user").value;
+    var passWord=document.getElementById("pass").value;
+    if(userName==='' && passWord==='') {
+    alert('Please Enter Credentials')
+    }
+    else if(userName==='' && passWord!==''){
+        alert('Please Enter User name')
+    }
+    else if(userName!=='' && passWord===''){
+    alert('Please Enter Password')
+    }
+    else if(userName==='Sabrudeen' && passWord==='Sabru@12'){
+        alert('Sucess')
+    }
+    else if(userName!=='Sabrudeen' || passWord!=='Sabru@12'){
+        alert('Invalid Credentials')
+    }
 }
+
+
+
+
